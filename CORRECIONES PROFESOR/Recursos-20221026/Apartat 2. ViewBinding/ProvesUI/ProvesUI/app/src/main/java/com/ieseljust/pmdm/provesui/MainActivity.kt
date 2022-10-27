0007ba13c84ca349f4d0c33fe294c68237b4c478
brunoulis <luisvapa2012@gmail.com>
2022-10-27 17:56:35 +0200

    27/10/22

--- CORRECIONES PROFESOR/Recursos-20221026/Apartat 2. ViewBinding/ProvesUI/ProvesUI/app/src/main/java/com/ieseljust/pmdm/provesui/MainActivity.kt
+++ CORRECIONES PROFESOR/Recursos-20221026/Apartat 2. ViewBinding/ProvesUI/ProvesUI/app/src/main/java/com/ieseljust/pmdm/provesui/MainActivity.kt
@@ -9,7 +9,7 @@ import com.ieseljust.pmdm.provesui.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
