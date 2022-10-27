03063650bbe4294ca8c8e00ee16830f5d0f8b71e
brunoulis <luisvapa2012@gmail.com>
2022-10-27 17:55:44 +0200

    27/10/22

--- APAC2BRUNOLUISVAZQUEZPAIS/app/src/main/java/com/ieseljust/brunoluisvazquezpais/apac2/MainActivity.kt
+++ APAC2BRUNOLUISVAZQUEZPAIS/app/src/main/java/com/ieseljust/brunoluisvazquezpais/apac2/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ieseljust.pmdm.apac2.Incidencia
 import com.ieseljust.pmdm.apac2.Incidencies
+import com.ieseljust.pmdm.apac2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 //cogemos la lista de incidencies y la transformamos en un lista mutable
     //para poder modificarla y añadir incidencias
     private var incidencies:MutableList<Incidencia> = Incidencies.incidencies.toMutableList()
-
+    private lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
