981d848d3d0d9bf3d24d758d70636420987637a3
brunoulis <luisvapa2012@gmail.com>
2022-11-21 15:56:13 +0100

    21/11/22

---
+++ APAC2BRUNOLUISVAZQUEZPAIS/APAC2BRUNOLUISVAZQUEZPAIS/app/src/main/java/com/ieseljust/brunoluisvazquezpais/apac2/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.ieseljust.brunoluisvazquezpais.apac2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ieseljust.brunoluisvazquezpais.apac2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    //cogemos la lista de incidencies y la transformamos en un lista mutable
+    //para poder modificarla y añadir incidencias
+    private lateinit var binding: ActivityMainBinding
+    private var incidenciaToRemove: Incidencia?=null
+
+
+
+    private fun itemClicked(incidencia: Incidencia, v: View) {
+        //Creamos un intent para abrir la actividad de edicion
+        val intent = Intent(this, EditaIncidencia::class.java).apply{
+
+            putExtra("package com.ieseljust.brunoluisvazquezpais.apac2.Incidencia",incidencia)
+        }
+        //Iniciamos la actividad
+        startActivity(intent)
+    }
+    private  fun itemLongClicked(incidencia: Incidencia, v: View):Boolean{
+        incidenciaToRemove=incidencia
+        val myDialog=MyDialogFragment(getString(R.string.askRemoveTitle),getString(R.string.askRemove))
+        myDialog.show(supportFragmentManager,"removeDialog")
+        return true
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Asociamos el layout de la incidencia a la lista
+        binding.IncidenciesRecyclerView.layoutManager= LinearLayoutManager(this)
+        binding.IncidenciesRecyclerView.setHasFixedSize(true)
+
+        binding.IncidenciesRecyclerView.adapter=AdaptadorIncidencias(
+            { incidencia: Incidencia, v: View -> itemClicked(incidencia, v) },
+            { incidencia: Incidencia, v: View -> itemLongClicked(incidencia, v)}
+        )
+
+        
+
+        
+
+
+    }
+}
